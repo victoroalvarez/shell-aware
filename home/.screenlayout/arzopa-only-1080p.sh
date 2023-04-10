8eb33ec1b5d9dd91592099b3962d5c38fab735fb
Victor Alvarez <victoroalvarez@protonmail.com>
2023-04-10 15:12:07 -0400

    Add scripts for monitor setup. Add modules to i3 configuration. Improve i3 configuration.

--- home/.screenlayout/arzopa-only-1080p.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --off --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off

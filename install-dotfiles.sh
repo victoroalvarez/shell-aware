ef54d48ca34a9533a1985204c5dfa685e94d6b8b
Victor O. Alvarez <victoroalvarez@protonmail.com>
2023-10-13 21:29:07 -0400

    Add document tree of the src/ directory. Improve install script. Add aliases. Improve Sway configuration. Add packages to debian 12 packages list. Add Zshenv file.

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -5,6 +5,7 @@
 # Create a /bin & /builds directory on $HOME directory
 mkdir $HOME/bin
 mkdir $HOME/builds
+mkdir $HOME/scripts
 
 # Create zsh files
 touch $HOME/.zsh_history
@@ -14,3 +15,9 @@ stow --target=$HOME src
 
 # Install Debian software packages
 #sudo xargs apt install -y < ./src/.packages-lists/debian12-packages.txt
+
+# setup tlp
+#sudo systemctl enable tlp.service
+#sudo systemctl mask systemd-rfkill.service
+#sudo systemctl mask systemd-rfkill.socket
+#sudo systemctl enable NetworkManager-dispatcher.service

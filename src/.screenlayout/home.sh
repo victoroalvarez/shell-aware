aa44198dc6250326208c15d777fc4c6b399209f1
Victor O. Alvarez <voa@keemail.me>
2025-01-07 19:45:13 -0400

    Improve i3 config. Add packages to list. Imrpove screen configuration scripts.

--- src/.screenlayout/home.sh
+++ src/.screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off
+xrandr --output eDP-1 --mode 1366x768 --pos 525x1080 --rotate normal --output HDMI-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off

9dc1ac5691bb2770ffaee401e7195ee3e496e74b
Victor O. Alvarez <victoroalvarez@protonmail.com>
2025-09-06 23:35:30 -0400

    Add Scripts and aliases.

--- src/.screenlayout/home.sh
+++ src/.screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1366x768 --pos 525x1080 --rotate normal --output HDMI-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+xrandr --output eDP-1 --primary --mode 1366x768 --pos 2560x0 --rotate normal --output HDMI-1 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
